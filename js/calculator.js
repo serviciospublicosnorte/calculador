5089d2d933be3527dd203053e8ac47ccfd1c021b
serviciospublicosnorte <serviciospublicosnorte.webmaster@outlook.com>
2025-04-07 16:49:19 -0300

    En la rama principal  Tu rama está actualizada con 'origin/principal'.
     Cambios a ser confirmados:
    	modificados:     css/style.css
    	modificados:     index.html
    	modificados:     js/calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -46,7 +46,7 @@ function calculateVolume() {
             : `${metrostoFixed(2)}%`; // Dos cecimales para números flotantes
                  
         resultElement.innerHTML = `
-    <div class="dato-resultado"><img class="warning-sing" src="images/attention.svg" alt="Signo de alerta"><span class="cantidad-span" id="texto-atención" style="color: #c43f35">¡Altura ingresada excede capacidad!</span></div>
+    <div class="dato-resultado-warning"><img class="warning-sing" src="images/attention.svg" alt="Signo de alerta"><span class="cantidad-span" id="texto-atención" style="color: #c43f35">¡Altura ingresada excede capacidad!</span></div>
     <div class="dato-resultado"><span class="cantidad-texto-span">Máxima altura</span><span class="cantidad-span" style="color: #284173">${metros} metros</span></div>
     <div class="dato-resultado"><span class="cantidad-texto-span">Capacidad del tanque</span><span class="cantidad-span" style="color: #ce813c">${fullVolumeLiters.toFixed(0).toLocaleString().trim()} litros</span></div>
     `;
