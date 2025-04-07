d8d45697f5dbba3ef1cbb94e56274a8b52a9875b
serviciospublicosnorte <serviciospublicosnorte.webmaster@outlook.com>
2025-04-06 21:49:42 -0300

    En la rama principal  Tu rama está actualizada con 'origin/principal'.
     Cambios a ser confirmados:
    	modificados:     js/calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -47,8 +47,8 @@ function calculateVolume() {
                  
         resultElement.innerHTML = `
     <div class="dato-resultado"><img src="images/attention.svg" alt="Signo de alerta"><span class="cantidad-span" style="color: red">¡La altura ingresada excede la capacidad del tanque!</span></div>
-    <div class="dato-resultado"><span class="cantidad-texto-span">Máxima altura</span><span class="cantidad-span" style="color: green">${metros} metros</span></div>
-    <div class="dato-resultado"><span class="cantidad-texto-span">Capacidad del tanque</span><span class="cantidad-span" style="color: blue">${fullVolumeLiters.toFixed(0).toLocaleString().trim()} litros</span></div>
+    <div class="dato-resultado"><span class="cantidad-texto-span">Máxima altura</span><span class="cantidad-span" style="color: blue">${metros} metros</span></div>
+    <div class="dato-resultado"><span class="cantidad-texto-span">Capacidad del tanque</span><span class="cantidad-span" style="color: green">${fullVolumeLiters.toFixed(0).toLocaleString().trim()} litros</span></div>
     `;
     } else {
         const percentageOccupied = (currentVolumeM3 / fullVolumeM3) * 100;
