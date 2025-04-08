30d252c63b587899091e1a7132cb2351913cf000
serviciospublicosnorte <serviciospublicosnorte.webmaster@outlook.com>
2025-04-07 22:52:39 -0300

    En la rama principal  Tu rama está actualizada con 'origin/principal'.
     Cambios a ser confirmados:
    	modificados:     css/style.css
    	modificados:     index.html
    	modificados:     js/calculator.js
    	borrados:        js/footer.js
    	nuevos archivos: js/warning.js

---
+++ js/warning.js
@@ -0,0 +1,29 @@
+function syncHeights() {
+    const resultadoElements = document.querySelectorAll('.dato-resultado');
+    const warningElements = document.querySelectorAll('.dato-resultado-warning');
+
+    if (resultadoElements.length > 0) {
+        // Obtiene la altura del elemento "resultado" (máx altura de ser necesario)
+        const resultadoHeight = resultadoElements[0].offsetHeight + 'px';
+
+        // Cambia la altura a "dato-resultado-warning"
+        warningElements.forEach(el => {
+            el.style.height = resultadoHeight;
+        });
+    }
+}
+
+// Arranca en la carga inicial
+document.addEventListener('DOMContentLoaded', syncHeights);
+
+// Chequea si hubo cambios en el DOM
+const observer = new MutationObserver(syncHeights);
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true, // En caso de que haya cambios
+    attributeFilter: ['class'] // Verifica cambio de clases
+});
+
+// También sincroniza cuando cambia el tamaño de la ventana
+window.addEventListener('resize', syncHeights);
